ceb33068c3aee46b21a1c9fa6dbdbed93f56dcab
glasshandstudios <brandon@glasshandstudios.com>
2018-11-05 16:46:44 -0500

    no message

---
+++ Practice_2D_01/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player; 
+    [SerializeField]
+    private float speed; 
+    //make a firing cooldown
+
+
+	
+	void Start () {
+
+        transform.position = new Vector3(0, 0, 0);
+        
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //when you press left click, fire
+		//when you press left, move left
+        //when you press right, move right
+        //when you press up, move up
+        //when you press down, move down
+	}
+}
