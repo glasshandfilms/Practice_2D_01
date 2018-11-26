e9d51d31b58c196a3e569a5eaa81a7cae6206afd
glasshandstudios <brandon@glasshandstudios.com>
2018-11-25 22:08:43 -0500

    implemented character controller animation, post processing, thrusters

---
+++ Practice_2D_01/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+	
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Turn_Left", true);
+            anim.SetBool("Turn_Right", false);
+        }	
+
+        else if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Turn_Left", false);
+            anim.SetBool("Turn_Right", false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetBool("Turn_Right", true);
+            anim.SetBool("Turn_Left", false);
+
+        }
+
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetBool("Turn_Right", false);
+            anim.SetBool("Turn_Left", false);
+        }
+
+    }
+}
